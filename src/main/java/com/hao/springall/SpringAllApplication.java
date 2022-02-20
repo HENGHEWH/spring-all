c5ad9c2a7babc8914a8a2039a65bda01d193835f
“HENGHEWH” <1836688128@qq.com>
2022-02-20 16:02:04 +0800

    创建spring整合项目

---
+++ src/main/java/com/hao/springall/SpringAllApplication.java
@@ -0,0 +1,14 @@
+package com.hao.springall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAllApplication.class, args);
+        System.out.println("hello world");
+    }
+
+}
---
+++ src/test/java/com/hao/springall/SpringAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hao.springall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
